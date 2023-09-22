1f568561f8ccac0f70dbd1fa45538171565052d5
yousufislam191 <yousufislam337@gmail.com>
2023-09-23 01:34:50 +0600

    setup cookie for production

--- server/helper/cookie.js
+++ server/helper/cookie.js
@@ -1,8 +1,10 @@
+const { cookieProduction } = require("../secret");
+
 const setAccessTokenCookie = (res, accessToken) => {
   res.cookie("accessToken", accessToken, {
     maxAge: 5 * 60 * 1000, // 5 minutes
     httpOnly: true,
-    secure: true,
+    secure: cookieProduction === "production",
     sameSite: "none",
     path: "/",
   });
@@ -12,7 +14,7 @@ const setRefreshTokenCookie = (res, refreshToken) => {
   res.cookie("refreshToken", refreshToken, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    secure: true,
+    secure: cookieProduction === "production",
     sameSite: "none",
     path: "/",
   });
