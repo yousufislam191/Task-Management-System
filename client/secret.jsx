f11ea8eb5b03c5acee2cb067f4f288148a33a8c9
yousufislam191 <yousufislam337@gmail.com>
2023-09-22 18:51:37 +0600

    update cloud server apiHostName

--- client/secret.jsx
+++ client/secret.jsx
@@ -5,7 +5,7 @@ let apiHostName;
 if (window.location.hostname === "localhost") {
   apiHostName = "http://localhost:5100/api"; // Set your local API URL here
 } else {
-  apiHostName = ""; // Set your Vercel API URL here
+  apiHostName = "https://task-management-system-eg5a.vercel.app/api"; // Set your Vercel API URL here
 }
 
 export default apiHostName;
