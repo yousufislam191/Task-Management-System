0fe6d4eb74e6f1dfb3ac0523e44870c2d70a033d
yousufislam191 <yousufislam337@gmail.com>
2023-09-23 06:08:38 +0600

    token bug checking

--- server/helper/cookie.js
+++ server/helper/cookie.js
@@ -3,7 +3,7 @@ const setAccessTokenCookie = (res, accessToken) => {
     maxAge: 5 * 60 * 1000, // 5 minutes
     httpOnly: true,
     secure: true,
-    domain: ".vercel.app",
+    // domain: ".vercel.app",
     sameSite: "none",
     path: "/",
   });
@@ -14,7 +14,7 @@ const setRefreshTokenCookie = (res, refreshToken) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
     secure: true,
-    domain: ".vercel.app",
+    // domain: ".vercel.app",
     sameSite: "none",
     path: "/",
   });
