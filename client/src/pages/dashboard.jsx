e0ccbe809bc3e578adefe2ef2c11845e6ba95505
yousufislam191 <yousufislam337@gmail.com>
2023-09-24 00:29:12 +0600

    fix catch error for refresh api page

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -22,12 +22,16 @@ import Tasks from "../components/Tasks";
 import Profile from "../components/Profile";
 import Chart from "../components/Chart";
 import Loading from "../components/Loading";
+import ErrorMessage from "../components/ErrorMessage";
 
 axios.defaults.withCredentials = true;
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const [isToken, setIsToken] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState();
+  const [status, setStatus] = useState();
+  const [errorMessage, setErrorMessage] = useState("");
   const [open, setOpen] = useState(true);
   const { setUser } = useUserContext();
   const { activeComponent } = useAppContext();
@@ -55,57 +59,63 @@ const Dashboard = () => {
         withCredentials: true,
       });
       if (res.data.success === true) {
-        setIsToken(true);
+        setLoading(true);
         setUser(res.data.payload);
       }
     } catch (err) {
-      setIsToken(true);
-      notify(err.response.status, err.response.data.message);
+      setLoading(true);
+      setStatus(err.response.status);
+      setSuccess(err.response.data.success);
+      setErrorMessage(err.response.data.message);
     }
   };
 
   useEffect(() => {
-    setIsToken(false);
+    setLoading(false);
     checkAccessToken();
   }, []);
 
-  return isToken ? (
-    <>
-      <ToastContainer />
+  return loading ? (
+    success === false ? (
+      <ErrorMessage status={status} message={errorMessage} />
+    ) : (
+      <>
+        <ToastContainer />
 
-      <ThemeProvider theme={theme}>
-        <Box sx={{ display: "flex" }}>
-          <CssBaseline />
-          <DashboardAppBar
-            open={open}
-            onToggleDrawer={toggleDrawer}
-            onLogoutButtonClick={handleLogoutButtonClick}
-          />
-          <SideNavBar open={open} onToggleDrawer={toggleDrawer} />
-          <Box
-            component="main"
-            sx={{
-              backgroundColor: (theme) =>
-                theme.palette.mode === "light"
-                  ? theme.palette.grey[100]
-                  : theme.palette.grey[900],
-              flexGrow: 1,
-              height: "100vh",
-              overflow: "auto",
-            }}
-          >
-            <Toolbar />
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-              {activeComponent === "Dashboard" && <Chart />}
-              {activeComponent === "Tasks" && <Tasks />}
-              {activeComponent === "Manage Users" && <ManageUser />}
-              {activeComponent === "Profile" && <Profile />}
-            </Container>
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}></Container>
+        <ThemeProvider theme={theme}>
+          <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <DashboardAppBar
+              open={open}
+              onToggleDrawer={toggleDrawer}
+              onLogoutButtonClick={handleLogoutButtonClick}
+            />
+            <SideNavBar open={open} onToggleDrawer={toggleDrawer} />
+            <Box
+              component="main"
+              sx={{
+                backgroundColor: (theme) =>
+                  theme.palette.mode === "light"
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[900],
+                flexGrow: 1,
+                height: "100vh",
+                overflow: "auto",
+              }}
+            >
+              <Toolbar />
+              <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                {activeComponent === "Dashboard" && <Chart />}
+                {activeComponent === "Tasks" && <Tasks />}
+                {activeComponent === "Manage Users" && <ManageUser />}
+                {activeComponent === "Profile" && <Profile />}
+              </Container>
+              <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}></Container>
+            </Box>
           </Box>
-        </Box>
-      </ThemeProvider>
-    </>
+        </ThemeProvider>
+      </>
+    )
   ) : (
     <Loading />
   );
