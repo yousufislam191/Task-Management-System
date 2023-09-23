62300832facc6c8f9abca9d6e51658bd7a723178
yousufislam191 <yousufislam337@gmail.com>
2023-09-23 22:46:06 +0600

    finally cookie issue solved

--- server/helper/cookie.js
+++ server/helper/cookie.js
@@ -3,7 +3,6 @@ const setAccessTokenCookie = (res, accessToken) => {
     maxAge: 5 * 60 * 1000, // 5 minutes
     httpOnly: true,
     secure: true,
-    // domain: ".vercel.app",
     sameSite: "none",
     path: "/",
   });
@@ -14,7 +13,6 @@ const setRefreshTokenCookie = (res, refreshToken) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
     secure: true,
-    // domain: ".vercel.app",
     sameSite: "none",
     path: "/",
   });
