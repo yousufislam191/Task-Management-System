d9a22ec21395cbe317a687b5769d4e549ccff45f
yousufislam191 <yousufislam337@gmail.com>
2023-09-30 02:58:56 +0600

    user profile delete added

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,16 +1,120 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Backdrop,
+  Box,
+  Button,
+  CircularProgress,
+  Fade,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 import { useUserContext } from "../context/UserContext";
+import FullWidthSubmitButton from "./FullWidthSubmitButton";
+import FullWidthLoadingButton from "./FullWidthLoadingButton";
+import { ToastContainer } from "react-toastify";
+import { useFormik } from "formik";
+import axios from "axios";
+import * as Yup from "yup";
+import showToast from "./showToast";
+import apiHostName from "../../secret";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
   const [isEditable, setIsEditable] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const [editedDetails, setEditedDetails] = useState();
+  const [openPasswordField, setOpenPasswordField] = useState(false);
   const { user } = useUserContext();
+  const userId = user.id;
+
+  const notify = (status, message) => showToast(status, message);
+
+  const userSchema = Yup.object({
+    oldPassword: Yup.string()
+      .required("Old password is required")
+      .min(8, "Old password must be at least 8 characters")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+        "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+      ),
+    newPassword: Yup.string()
+      .required("Old password is required")
+      .min(8, "Old password must be at least 8 characters")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+        "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+      ),
+    confirmPassword: Yup.string()
+      .required("Old password is required")
+      .oneOf(
+        [Yup.ref("newPassword"), null],
+        "Confirm password does not match with New password"
+      ),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      oldPassword: "",
+      newPassword: "",
+      confirmPassword: "",
+    },
+    onSubmit: async (values, helpers) => {
+      setLoading(false);
+      try {
+        const res = await axios.put(
+          `${apiHostName}/user/update-password/${userId}`,
+          {
+            oldPassword: values.oldPassword,
+            newPassword: values.newPassword,
+            confirmPassword: values.confirmPassword,
+          }
+        );
+        if (res.data.success === true) {
+          setLoading(true);
+          notify(res.status, res.data.message);
+          setOpenPasswordField(false);
+        }
+      } catch (err) {
+        setLoading(true);
+        notify(err.response.status, err.response.data.message);
+      }
+    },
+    validationSchema: userSchema,
+  });
+
+  const deleteAccount = async () => {
+    setLoading(false);
+    try {
+      const userId = user.id;
+      const res = await axios.delete(`${apiHostName}/user/${userId}`);
+      if (res.data.success) {
+        // for session and cookie clear
+        await axios.post(`${apiHostName}/auth/logout`).catch((err) => {
+          notify(err.response.status, err.response.data.message);
+        });
+
+        setLoading(true);
+        notify(res.status, res.data.message);
+        setTimeout(() => {
+          navigate("/");
+        }, 1500);
+      }
+    } catch (err) {
+      setLoading(true);
+      notify(err.response.status, err.response.data.message);
+    }
+  };
 
   const handleEdit = () => {
     setIsEditable(true);
     setEditedDetails({ ...user });
   };
+
+  const handleModalOpen = () => setOpenModal(true);
+  const handleModalClose = () => setOpenModal(false);
 
   const handleSave = () => {
     setIsEditable(false);
@@ -18,8 +122,13 @@ const Profile = () => {
     console.log(editedDetails.name);
   };
 
+  const handleOpenPasswordField = () => {
+    setOpenPasswordField(!openPasswordField);
+  };
+
   return (
     <>
+      <ToastContainer />
       <div style={{ width: "100%" }}>
         <Box
           sx={{
@@ -38,38 +147,24 @@ const Profile = () => {
         <Box
           sx={{
             display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            mb: 3,
+            flexDirection: "column",
+            gap: "2rem",
+            "@media (min-width: 600px)": {
+              flexDirection: "row",
+            },
           }}
         >
-          <TextField
-            label="Name"
-            value={isEditable ? editedDetails.name || "" : user.name || ""}
-            variant="outlined"
-            disabled={!isEditable}
-            onChange={(e) =>
-              setEditedDetails({ ...user, name: e.target.value })
-            }
-          />
-          {!isEditable && (
-            <Button
-              variant="text"
-              disableElevation
-              onClick={handleEdit}
-              sx={{
-                textTransform: "capitalize",
-                alignItems: "left",
-                backgroundColor: "transparent",
-                "&:hover": {
-                  backgroundColor: "transparent",
-                },
-              }}
-            >
-              Edit
-            </Button>
-          )}
-          {isEditable && (
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              gap: 3,
+              "@media (min-width: 600px)": {
+                width: "50%",
+              },
+            }}
+          >
             <Box
               sx={{
                 display: "flex",
@@ -77,56 +172,254 @@ const Profile = () => {
                 alignItems: "center",
               }}
             >
+              <TextField
+                fullWidth
+                label="Name"
+                value={isEditable ? editedDetails.name || "" : user.name || ""}
+                variant="outlined"
+                disabled={!isEditable}
+                onChange={(e) =>
+                  setEditedDetails({ ...user, name: e.target.value })
+                }
+              />
+              {!isEditable && (
+                <Button
+                  variant="text"
+                  disableElevation
+                  onClick={handleEdit}
+                  sx={{
+                    textTransform: "capitalize",
+                    alignItems: "left",
+                    backgroundColor: "transparent",
+                    "&:hover": {
+                      backgroundColor: "transparent",
+                    },
+                  }}
+                >
+                  Edit
+                </Button>
+              )}
+              {isEditable && (
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <Button
+                    variant="text"
+                    disableElevation
+                    onClick={handleSave}
+                    sx={{
+                      textTransform: "capitalize",
+                      alignItems: "left",
+                      backgroundColor: "transparent",
+                      "&:hover": {
+                        backgroundColor: "transparent",
+                      },
+                    }}
+                  >
+                    Save
+                  </Button>
+                  <Button
+                    variant="text"
+                    disableElevation
+                    onClick={() => setIsEditable(false)}
+                    sx={{
+                      textTransform: "capitalize",
+                      alignItems: "left",
+                      backgroundColor: "transparent",
+                      "&:hover": {
+                        backgroundColor: "transparent",
+                      },
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                </Box>
+              )}
+            </Box>
+
+            <TextField
+              fullWidth
+              label="Email"
+              value={user.email}
+              variant="outlined"
+              disabled
+            />
+
+            {openPasswordField && (
+              <form onSubmit={formik.handleSubmit}>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
+                  <TextField
+                    fullWidth
+                    label="Old Password"
+                    name="oldPassword"
+                    id="oldPassword"
+                    type="text"
+                    autoComplete="password"
+                    variant="outlined"
+                    error={formik.errors.oldPassword}
+                    onChange={formik.handleChange}
+                    helperText={formik.errors.oldPassword}
+                    autoFocus
+                  />
+                  <TextField
+                    fullWidth
+                    label="New Password"
+                    name="newPassword"
+                    id="newPassword"
+                    type="text"
+                    autoComplete="password"
+                    variant="outlined"
+                    error={formik.errors.newPassword}
+                    onChange={formik.handleChange}
+                    helperText={formik.errors.newPassword}
+                    autoFocus
+                  />
+                  <TextField
+                    fullWidth
+                    label="Confirm Password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    type="text"
+                    autoComplete="password"
+                    variant="outlined"
+                    error={formik.errors.confirmPassword}
+                    onChange={formik.handleChange}
+                    helperText={formik.errors.confirmPassword}
+                    autoFocus
+                  />
+                  {loading ? (
+                    <FullWidthSubmitButton text={"Save Change Password"} />
+                  ) : (
+                    <FullWidthLoadingButton />
+                  )}
+                </Box>
+              </form>
+            )}
+          </Box>
+
+          <Box
+            sx={{
+              width: "100%",
+              "@media (min-width: 600px)": {
+                width: "50%",
+              },
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "start",
+                gap: "2rem",
+                mb: 3,
+                "@media (min-width: 600px)": {
+                  flexDirection: "column",
+                  gap: "1rem",
+                  alignItems: "end",
+                },
+                "@media (min-width: 1024px)": {
+                  flexDirection: "row",
+                  gap: "2rem",
+                  justifyContent: "end",
+                },
+              }}
+            >
               <Button
-                variant="text"
-                disableElevation
-                onClick={handleSave}
+                variant="contained"
+                onClick={handleOpenPasswordField}
                 sx={{
                   textTransform: "capitalize",
-                  alignItems: "left",
-                  backgroundColor: "transparent",
-                  "&:hover": {
-                    backgroundColor: "transparent",
-                  },
                 }}
               >
-                Save
+                {openPasswordField ? "Close Password" : "Change Password"}
               </Button>
               <Button
-                variant="text"
-                disableElevation
-                onClick={() => setIsEditable(false)}
+                variant="contained"
+                color="error"
+                onClick={handleModalOpen}
                 sx={{
                   textTransform: "capitalize",
-                  alignItems: "left",
-                  backgroundColor: "transparent",
-                  "&:hover": {
-                    backgroundColor: "transparent",
-                  },
                 }}
               >
-                Cancel
+                Delete Account
               </Button>
             </Box>
-          )}
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            mb: 3,
-          }}
-        >
-          <TextField
-            label="Email"
-            value={user.email}
-            variant="outlined"
-            disabled
-          />
+          </Box>
         </Box>
       </div>
+
+      {openModal && (
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          closeAfterTransition
+          open={openModal}
+          onClose={handleModalClose}
+          slots={{ backdrop: Backdrop }}
+          slotProps={{
+            backdrop: {
+              timeout: 500,
+            },
+          }}
+        >
+          <Fade in={openModal}>
+            <Box
+              sx={{
+                position: "absolute",
+                bgcolor: "background.paper",
+                boxShadow: 24,
+                p: 4,
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                borderRadius: 2,
+                "@media (max-width: 600px)": {
+                  width: "90%",
+                },
+              }}
+            >
+              <Typography
+                id="transition-modal-title"
+                variant="h6"
+                component="h2"
+                color="error"
+              >
+                Are you sure to delete your account?
+              </Typography>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: 2,
+                  justifyContent: "end",
+                  alignContent: "center",
+                  mt: 3,
+                }}
+              >
+                <Button variant="contained" onClick={handleModalClose}>
+                  No
+                </Button>
+                {loading ? (
+                  <Button
+                    variant="contained"
+                    color="error"
+                    onClick={deleteAccount}
+                  >
+                    Yes
+                  </Button>
+                ) : (
+                  <CircularProgress size={20} />
+                )}
+              </Box>
+            </Box>
+          </Fade>
+        </Modal>
+      )}
     </>
   );
 };
