b520e342da80b58ed3ebcace6cd42cf0e108dfaa
yousufislam191 <yousufislam337@gmail.com>
2023-09-30 04:59:21 +0600

    added active component color

--- client/src/components/ListItems.jsx
+++ client/src/components/ListItems.jsx
@@ -10,7 +10,7 @@ import { useAppContext } from "../context/AppContext";
 import { useUserContext } from "../context/UserContext";
 
 const ListItems = () => {
-  const { setActiveComponent } = useAppContext();
+  const { activeComponent, setActiveComponent } = useAppContext();
   const { user } = useUserContext();
 
   const handleItemClick = (componentName) => {
@@ -19,31 +19,71 @@ const ListItems = () => {
 
   return (
     <>
-      {/* <ListItemButton onClick={() => handleItemClick("Dashboard")}>
+      {/* <ListItemButton
+        onClick={() => handleItemClick("Dashboard")}
+        selected={activeComponent === "Dashboard"}
+      >
         <ListItemIcon>
-          <DashboardIcon />
+          <DashboardIcon
+            color={activeComponent === "Dashboard" ? "primary" : "inherit"}
+          />
         </ListItemIcon>
-        <ListItemText primary="Dashboard" />
+        <ListItemText
+          primary="Dashboard"
+          primaryTypographyProps={{
+            color: activeComponent === "Dashboard" ? "primary" : "initial",
+          }}
+        />
       </ListItemButton> */}
-      <ListItemButton onClick={() => handleItemClick("Tasks")}>
+      <ListItemButton
+        onClick={() => handleItemClick("Tasks")}
+        selected={activeComponent === "Tasks"}
+      >
         <ListItemIcon>
-          <AssignmentIcon />
+          <AssignmentIcon
+            color={activeComponent === "Tasks" ? "primary" : "inherit"}
+          />
         </ListItemIcon>
-        <ListItemText primary="Tasks" />
+        <ListItemText
+          primary="Tasks"
+          primaryTypographyProps={{
+            color: activeComponent === "Tasks" ? "primary" : "initial",
+          }}
+        />
       </ListItemButton>
       {user?.isAdmin === true && (
-        <ListItemButton onClick={() => handleItemClick("Manage Users")}>
+        <ListItemButton
+          onClick={() => handleItemClick("Manage Users")}
+          selected={activeComponent === "Manage Users"}
+        >
           <ListItemIcon>
-            <PeopleIcon />
+            <PeopleIcon
+              color={activeComponent === "Manage Users" ? "primary" : "inherit"}
+            />
           </ListItemIcon>
-          <ListItemText primary="Manage Users" />
+          <ListItemText
+            primary="Manage Users"
+            primaryTypographyProps={{
+              color: activeComponent === "Manage Users" ? "primary" : "initial",
+            }}
+          />
         </ListItemButton>
       )}
-      <ListItemButton onClick={() => handleItemClick("Profile")}>
+      <ListItemButton
+        onClick={() => handleItemClick("Profile")}
+        selected={activeComponent === "Profile"}
+      >
         <ListItemIcon>
-          <AccountCircleIcon />
+          <AccountCircleIcon
+            color={activeComponent === "Profile" ? "primary" : "inherit"}
+          />
         </ListItemIcon>
-        <ListItemText primary="Profile" />
+        <ListItemText
+          primary="Profile"
+          primaryTypographyProps={{
+            color: activeComponent === "Profile" ? "primary" : "initial",
+          }}
+        />
       </ListItemButton>
     </>
   );
